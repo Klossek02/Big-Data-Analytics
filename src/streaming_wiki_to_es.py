8b3e6ee4360c7a31a64afb4163e5e9d2fcb0bf22
Aleksandra Kłos <67869082+Klossek02@users.noreply.github.com>
2026-01-18 17:26:33 +0100

    update file to streaming Wikipedia content to ElasticSearch

--- src/streaming_wiki_to_es.py
+++ src/streaming_wiki_to_es.py
@@ -5,8 +5,11 @@ from elasticsearch import Elasticsearch
 
 KAFKA_TOPIC = "wikipedia.edits"  
 KAFKA_SERVER = "localhost:9092"
-ES_HOST = "http://localhost:9200"
+ES_HOST = "https://localhost:9200"
 ES_INDEX = "wikipedia_real_data"  
+ES_USER = "elastic"
+ES_PASS = "jCIDwLC=+xE12BVMqBK5"
+#ES_CA_CERT = "/etc/elasticsearch/certs/http_ca.crt"  
 
 def send_to_es(batch_df, batch_id):
     if batch_df.count() == 0:
@@ -16,7 +19,16 @@ def send_to_es(batch_df, batch_id):
     records = batch_df.collect()
 
     try:
-        es = Elasticsearch(ES_HOST)
+        es = Elasticsearch(
+            [ES_HOST],
+            basic_auth=(ES_USER, ES_PASS),
+            verify_certs=False,      
+            ssl_show_warn=False
+            #ca_certs=ES_CA_CERT
+        )
+        
+        success_count = 0
+    
         for row in records:
             doc = {
                 "user": row.user,
